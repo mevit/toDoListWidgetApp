12bed1b3fa18da22ed1b0e6aac08aa9b57c61931
Vitaliy Melnyk <vitalkamelnyk@gmail.com>
2019-09-18 00:34:42 +0300

    T-3,T-4,T-5,T-6,T-7. Set color for SplashScreen. Add toolbar to MainFragment. Add button for go to EditFragment. Add recyclerView to MainFragment

--- app/src/main/java/com/meldeveloping/todowidget/main/MainFragment.kt
+++ app/src/main/java/com/meldeveloping/todowidget/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.meldeveloping.todowidget.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -13,6 +14,26 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initNewButton()
+    }
+
+    private fun initNewButton(){
+        goToEditButton.setOnClickListener {
+            goToEditFragment()
+        }
+    }
+
+    private fun goToEditFragment(){
+        fragmentManager!!
+            .beginTransaction()
+            .replace(R.id.mainContainer, EditFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
 
     companion object {
         @JvmStatic
